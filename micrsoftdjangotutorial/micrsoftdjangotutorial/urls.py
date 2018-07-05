f9d145a01a5ae0b5ec4c9c34b3e74e3210f8221b
max <maximilianestrada@yahoo.com>
2018-07-05 18:01:28 -0500

    Add project files.

---
+++ micrsoftdjangotutorial/micrsoftdjangotutorial/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for micrsoftdjangotutorial.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', micrsoftdjangotutorial.views.home, name='home'),
+    # url(r'^micrsoftdjangotutorial/', include('micrsoftdjangotutorial.micrsoftdjangotutorial.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
