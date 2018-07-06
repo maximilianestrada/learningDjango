4b77721fbb093b29139326ffb0462068a220eb56
max <maximilianestrada@yahoo.com>
2018-07-06 11:58:00 -0500

    Added first django app

--- micrsoftdjangotutorial/micrsoftdjangotutorial/urls.py
+++ micrsoftdjangotutorial/micrsoftdjangotutorial/urls.py
@@ -3,12 +3,19 @@ Definition of urls for micrsoftdjangotutorial.
 """
 
 from django.conf.urls import include, url
+import MyApp1.views
+
+# Django processes URL patterns in the order they appear in the array
+urlpatterns = [
+    url(r'^$', MyApp1.views.index, name='index'),
+    url(r'^home$', MyApp1.views.index, name='home'),
+]
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = [
+# urlpatterns = [
     # Examples:
     # url(r'^$', micrsoftdjangotutorial.views.home, name='home'),
     # url(r'^micrsoftdjangotutorial/', include('micrsoftdjangotutorial.micrsoftdjangotutorial.urls')),
@@ -18,4 +25,4 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-]
+# ]
